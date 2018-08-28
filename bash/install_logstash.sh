1ea54a650614d830b9c58cad4d441255a92a839b
Roles_le <13051960278@163.com>
2018-08-28 14:38:53 +0800

    change logstash

--- bash/install_logstash.sh
+++ bash/install_logstash.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 set -e
 
-elk_user=elk
-useradd (){
-  useradd ${elk_user}
-  echo "123qwe!@#QWE" | passwd ${elk_user} --stdin
-  grep "${elk_user} ALL=(ALL) NOPASSWD: ALL" /etc/sudoers || echo "${elk_user} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-  grep "Defaults: ${elk_user}  !requiretty" /etc/sudoers || echo "Defaults: ${elk_user}  !requiretty" >> /etc/sudoers
+logstash_user=elk
+auseradd (){
+  useradd ${logstash_user}
+  echo "123qwe!@#QWE" | passwd ${logstash_user} --stdin
+  grep "${logstash_user} ALL=(ALL) NOPASSWD: ALL" /etc/sudoers || echo "${logstash_user} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+  grep "Defaults: ${logstash_user}  !requiretty" /etc/sudoers || echo "Defaults: ${logstash_user}  !requiretty" >> /etc/sudoers
 }
 
 install_logstash (){
-  if [ ! -d /home/${elk_user}/logstash-6.3.0 ]; then
+  if [ ! -d /home/${logstash_user}/logstash-6.3.0 ]; then
     wget -q -O /tmp/logstash-6.3.0.tar.gz https://artifacts.elastic.co/downloads/logstash/logstash-6.3.0.tar.gz
-    tar -zxf /tmp/logstash-6.3.0.tar.gz -C /home/${elk_user}
-    chown -R ${elk_user}.${elk_user} /home/${elk_user}
+    tar -zxf /tmp/logstash-6.3.0.tar.gz -C /home/${logstash_user}
+    chown -R ${logstash_user}.${logstash_user} /home/${logstash_user}
   else
-    echo "Directory /home/${elk_user}/logstash-6.3.0 exists"
+    echo "Directory /home/${logstash_user}/logstash-6.3.0 exists"
   fi
 }
 
-if [ 0 -eq $(grep ${elk_user} /etc/passwd | wc -l ) ]; then
-  useradd
-  echo "create elk_user ${elk_user}"
+if [ 0 -eq $(grep ${logstash_user} /etc/passwd | wc -l ) ]; then
+  auseradd
+  echo "create logstash_user ${logstash_user}"
 else 
-  echo "elk_user ${elk_user} exists"
+  echo "logstash_user ${logstash_user} exists"
 fi
 install_logstash
