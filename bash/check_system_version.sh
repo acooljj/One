5c1dd070b0fc039f0db0ab024cd654e49f2756fd
Roles_le <13051960278@163.com>
2018-08-30 17:22:35 +0800

    update check_system_version

--- bash/check_system_version.sh
+++ bash/check_system_version.sh
@@ -2,6 +2,7 @@
 
 Get_System_Name (){
     if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        system_version_num=$(uname -r | awk -F "el" '{print $2}' |  awk -F '.' '{print $1}')
         system_version='CentOS'
         PM='yum'
     elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
@@ -28,3 +29,5 @@ Get_System_Name (){
 }
 Get_System_Name
 echo  "System_version : ${system_version}"
+echo  "System_version_num : ${system_version_num}"
+
