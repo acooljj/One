333700d46a0e4be55527e09570d89df3d6067f25
Roles_le <13051960278@163.com>
2018-08-31 08:43:11 +0800

    add jdk install

--- bash/install_jenkins.sh
+++ bash/install_jenkins.sh
@@ -4,11 +4,14 @@ curl https://raw.githubusercontent.com/mainiubaba/One/master/bash/init | bash
 wget -O /tmp/check_system_version.sh https://raw.githubusercontent.com/mainiubaba/One/master/bash/check_system_version.sh --no-check-certificate
 source /tmp/check_system_version.sh
 
+#安装自动选择最快源的插件
+yum install yum-fastestmirror -y
+#添加Jenkins源
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
 fun_centos7 (){
-  yum -y install jenkins fontconfig
+  yum -y install jenkins fontconfig java
   systemctl start jenkins
 }
 
@@ -19,10 +22,12 @@ fun_centos6 (){
 
 fun_jenkins (){
   echo "#Jenkins configure : /etc/sysconfig/jenkins"
+  MyIP=$(ip r | grep $(ip r | grep default |awk '{print $5}') | grep -v default | awk '{print $9}' | head -1)
+  echo "#Serving HTTP on ${MyIP}  port 8080 ."
+  echo "#Placse wait AdminPassword generate ..."
+  while true; do if [ -f /var/lib/jenkins/secrets/initialAdminPassword ];then break; else sleep 3; fi; done
   jenkins_pass=$(cat /var/lib/jenkins/secrets/initialAdminPassword)
   echo "#Jenkins AdminPassword : ${jenkins_pass}"
-  MyIP=$(ip r | grep $(ip r | grep default |awk '{print $5}') | grep -v default | awk '{print $9}' | head -1)
-  echo "#Serving HTTP on ${MyIP}  port 8080 ..."
 }
 
 case ${system_version} in
