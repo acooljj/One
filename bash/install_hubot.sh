4fba86785d0e34fde1be0ca38424233f30315012
Roles_le <13051960278@163.com>
2018-09-03 13:37:06 +0800

    change hubot

--- bash/install_hubot.sh
+++ bash/install_hubot.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
-yum -y install wget
+rpm -qa | grep wget || yum -y install wget
 #安装node版本
 node_version=8.11.4
 #安装node路径
 node_home_path=/usr/local
+#Toekn ID
+Slack_Token=xxxx-000000000000-000000000000-xxxxxxxxxxxxxxxxxxxxxxxx
 
 function node_install (){
   echo "Install Node..."
@@ -17,6 +19,7 @@ function node_install (){
     grep "export NODE_HOME=${node_home_path}/node" /etc/profile || echo "export NODE_HOME=${node_home_path}/node" >> /etc/profile
     grep 'export PATH=$NODE_HOME/bin:$PATH' /etc/profile ||  echo 'export PATH=$NODE_HOME/bin:$PATH' >> /etc/profile
     source /etc/profile
+    npm i -g npm to update
     echo "node : $(node -v)"
     echo "npm : $(npm -v)"
     echo "Node Install Finished: SUCCESS"
@@ -25,49 +28,34 @@ function node_install (){
   fi
 }
 
-function Hubot_install (){
+function userad_node (){
+  if  grep -Eq "node" /etc/passwd ;then :;else useradd node;  echo "node" | passwd node --stdin;fi
+}
+
+function Hubot_install_yo (){
+  node_install
   echo "Install Hubot..."
-  npm install -g yo generator-hubot
-  if [ $USER == "root" ];then
-    chmod g+rwx -R /root /root/.config 
-    chmod 775 -R /root/.config/configstore/
-    chmod 775 -R /root/.npm/_cacache/
-    chmod 775 -R /root/.npm/_locks
-  else
-    :
-  fi
-  node_data=${node_home_path}/node/node_$USER_$(date '+%F')
-  mkdir ${node_data}
+  if type yo > /dev/null 2>&1; then :;else npm install -g yo generator-hubot;fi
+}
+
+function Hubot_install (){
+  export node_data=/home/node/Hubot/node-$(date '+%F')
+  if [ -d ${node_data} ];then :;else mkdir -pv ${node_data};fi
   chmod g+rwx ${node_data}
-  cd ${node_data}
-  echo "++根据提示填写相关信息: [Email, HuhotName, Bs]"
-  yo hubot --adapter=slack || chmod g+rwx -R /root/.config/insight-nodejs  && yo hubot --adapter=slack
+  chown node.node -R ${node_data}
+  echo "++根据提示填写相关信息: [Email, HuhotName, Bs]";
+  su - node -c "cd ${node_data} && yo hubot --adapter=slack"
   echo "Hubot Install Finished: SUCCESS"
   sleep 1
   echo "++输入机器人连接Slack的Token："
   echo "++Ps: [登陆slack，添加app，进入app商店搜索hubot，然后安装，如果已经安装就是可以添加机器人，下滑有这个机器人的Token值]"
-  while true;
-  do
-   read -p "++Token :" Slack_Token
-   Zoo=$(echo ${Slack_Token} | wc -L) #55
-   if [ ${Zoo} != 55 ];then break;fi
-   One=$(echo ${Slack_Token} | awk -F "-" '{print $1}' | wc -L) #4
-   Two=$(echo ${Slack_Token} | awk -F "-" '{print $2}' | wc -L) #12
-   Three=$(echo ${Slack_Token} | awk -F "-" '{print $3}' | wc -L) #12
-   Four=$(echo ${Slack_Token} | awk -F "-" '{print $4}' | wc -L) #24
-   if [ ! -z ${Slack_Token} ];then
-      if [ ${Zoo} -eq 55  ];then
-        if [ ${One} -eq 4 ];then
-          if [ ${Two} -eq 12 ];then
-            if [ ${Three} -eq 12 ];then
-              if [ ${Four} -eq 24 ];then echo "Slack_Token length is 55"; break;fi ;fi ;fi ;fi ;fi
-   else echo -n "Placse Input Token or Exit(Ctrl+c)";fi
-  done
   grep "export HUBOT_SLACK_TOKEN=" /etc/profile || echo "export HUBOT_SLACK_TOKEN=${Slack_Token}" >> /etc/profile
-  source /etc/profile
+  source /etc/profile > /dev/null
   echo "Start Hubot_slack..."
-  nohup hubot --adapter slack 2&>1 >> /dev/null &
+  su - node -c "cd ${node_data} && nohup ./bin/hubot --adapter slack 2&>1 >> /dev/null &"
 }
-node_install
+
+userad_node
+Hubot_install_yo
 Hubot_install
 
