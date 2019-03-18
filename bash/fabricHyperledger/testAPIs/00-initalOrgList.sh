6941f73e666048f6a3e5da0f334d712be2b899f4
Roles_le <13051960278@163.com>
2019-03-18 14:18:23 +0800

    add couchdb

--- bash/fabricHyperledger/testAPIs/00-initalOrgList.sh
+++ bash/fabricHyperledger/testAPIs/00-initalOrgList.sh
@@ -4,7 +4,7 @@ MaxNum=${1:-20}
 
 for i in `seq 1 ${MaxNum}`;
 do
-  echo "org${i};peer0 ${portNum}051,${portNum}053;peer1 ${portNum}056,${portNum}058" >> ~/orgList
+  echo "org${i};peer0 ${portNum}051,${portNum}053;peer1 ${portNum}056,${portNum}058;couchdb ${portNum}981;${portNum}986" >> ~/orgList
   portNum=$(expr $portNum + 1)
 done
 
