fa4acd1e00c734cc0c9997397fc4604ab8004e92
Roles_le <13051960278@163.com>
2019-04-02 12:08:59 +0800

    add view

--- bash/fabricHyperledger/04-CouchdbInit.sh
+++ bash/fabricHyperledger/04-CouchdbInit.sh
@@ -7,6 +7,11 @@ do
   port2=`echo $portlist | awk '{print $4}' | awk -F ';' '{print $1}' | awk -F ',' '{print $2}'`
   passw=`echo $portlist | awk '{print $4}' | awk -F ';' '{print $2}'`
   echo $port1 $port2 $passw
-  curl -X POST http://admin:$passw@localhost:$port1/mychannel_chain/_index -H "content-type: application/json" -d '{"index": {"fields": [ {"createTime": "desc"}]},"name":"createTime","type": "json"}'
+  if [ ${port1} == 7981 ];then
+	  curl -X POST http://localhost:$port1/mychannel_chain/_index -H "content-type: application/json" -d '{"index": {"fields": [ {"createTime": "desc"}]},"name":"createTime","type": "json"}'
+  else
+	  curl -X POST http://admin:$passw@localhost:$port1/mychannel_chain/_index -H "content-type: application/json" -d '{"index": {"fields": [ {"createTime": "desc"}]},"name":"createTime","type": "json"}'
+  fi
   curl -X POST http://admin:$passw@localhost:$port2/mychannel_chain/_index -H "content-type: application/json" -d '{"index": {"fields": [ {"createTime": "desc"}]},"name":"createTime","type": "json"}'
 done
+
